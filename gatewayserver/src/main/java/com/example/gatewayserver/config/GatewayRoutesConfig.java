46e067fe1e1158e7759ac7696f06474524bc9038
SergiuCarpinisan <sergiu.carpinisan.04@gmail.com>
2025-11-25 12:31:27 +0200

    added gateway server

---
+++ gatewayserver/src/main/java/com/example/gatewayserver/config/GatewayRoutesConfig.java
@@ -0,0 +1,16 @@
+package com.example.gatewayserver.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatewayRoutesConfig {
+
+    @Bean
+    public RouteLocator routes(RouteLocatorBuilder builder) {
+//        add routes
+        return null;
+    }
+}
